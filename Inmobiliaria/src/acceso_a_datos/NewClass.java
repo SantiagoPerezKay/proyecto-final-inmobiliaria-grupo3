1b990aad592c157d1cf37b08b5b74e5862882f8e
santiago nicolas perez kay <santiagoperezkay88@gmail.com>
2023-10-03 10:32:10 -0300

    proyecto-basico-netbeans

---
+++ Inmobiliaria/src/acceso_a_datos/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package acceso_a_datos;
+
+/**
+ *
+ * @author santy
+ */
+public class NewClass {
+    
+}
